e40bac2dbabddd9e0fe89c19cc6e72feb93939c2
Prakarsha-k <147191789+Prakarsha-k@users.noreply.github.com>
2025-01-03 21:47:36 +0530

    Time: 497 ms (10.9%), Space: 0B (100%) - LeetHub

---
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -0,0 +1,13 @@
+SELECT 
+    machine_id, 
+    ROUND(AVG(end_time - start_time), 3) AS processing_time
+FROM (
+    SELECT 
+        machine_id, 
+        process_id, 
+        MIN(timestamp) AS start_time, 
+        MAX(timestamp) AS end_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+) AS ProcessTimes
+GROUP BY machine_id;
