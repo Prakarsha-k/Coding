62b2b3dce4790d0d13f0ea6f98f2d67eda5b78a7
Prakarsha-k <147191789+Prakarsha-k@users.noreply.github.com>
2025-09-22 02:59:24 +0530

    Time: 0 ms (100%), Space: 17.8 MB (49.07%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,7 +1,11 @@
-class Solution(object):
-    def plusOne(self, digits):
-        digits1=int(''.join(map(str,digits)))
-        sum_digits=digits1+1
-        result = list(map(int, str(sum_digits)))
-        return result
-        
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
