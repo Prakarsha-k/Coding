d016f7a6b2c92e4736838218aeb8985711b98adc
Prakarsha-k <147191789+Prakarsha-k@users.noreply.github.com>
2025-09-21 19:00:28 +0530

    Time: 17 ms (78.15%), Space: 18.2 MB (85.89%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        parent = {}
+
+        def find(x):
+            if x != parent.setdefault(x, x):
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            parent[find(x)] = find(y)
+
+        for x, y in stones:
+            union(x, ~y)
+        return len(stones) - len({find(x) for x, y in stones})
