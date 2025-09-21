6629d1c7a556459a0231158c7f0af4429ab6d887
Prakarsha-k <147191789+Prakarsha-k@users.noreply.github.com>
2025-09-22 03:08:36 +0530

    Time: 5 ms (80.71%), Space: 21.1 MB (62.6%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        res = [0]
+        for i in range(n):
+            res += [x | (1 << i) for x in reversed(res)]
+        return res
