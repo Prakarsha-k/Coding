c0084e872f8decfc1ea7c09f6cd3d2ee321b9862
Prakarsha-k <147191789+Prakarsha-k@users.noreply.github.com>
2025-09-21 13:05:53 +0530

    Time: 18 ms (67.71%), Space: 45.4 MB (93.75%) - LeetHub

---
+++ 0868-push-dominoes/0868-push-dominoes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String pushDominoes(String dominoes) {
+        int n = dominoes.length();
+        char[] arr = dominoes.toCharArray();
+        int[] force = new int[n];
+        
+        int f = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 'R') f = n;
+            else if (arr[i] == 'L') f = 0;
+            else f = Math.max(f - 1, 0);
+            force[i] += f;
+        }
+        
+        f = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == 'L') f = n;
+            else if (arr[i] == 'R') f = 0;
+            else f = Math.max(f - 1, 0);
+            force[i] -= f;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (force[i] > 0) arr[i] = 'R';
+            else if (force[i] < 0) arr[i] = 'L';
+            else arr[i] = '.';
+        }
+        return new String(arr);
+    }
+}
