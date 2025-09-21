9634f59a8ee38e96a434900c122a1a56b183eed9
Prakarsha-k <147191789+Prakarsha-k@users.noreply.github.com>
2025-09-21 13:17:04 +0530

    Time: 2215 ms (5%), Space: 41.6 MB (18.39%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.py
@@ -0,0 +1,11 @@
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        cars = sorted(zip(position, speed), reverse=True)
+        times = [(target - p) / s for p, s in cars]
+        fleets = 0
+        while times:
+            lead = times.pop(0)
+            fleets += 1
+            while times and times[0] <= lead:
+                times.pop(0)
+        return fleets
